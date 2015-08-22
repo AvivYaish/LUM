3601443fc6ae2a38d1f440ca2264160b0a45500f
Aviv Yaish <aviv.yaish@mail.huji.ac.il>
2015-08-22 21:30:34 +0300

    moved generation of matrix markup to a different function

--- display.js
+++ display.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
     $("#decomposition").hide();
 });
 
+/**
+ * Draws the input table for the input matrix.
+ */
 function drawMatrixInput() {
     var size = $("#matrix-size").val();
     var tableMarkup = "";
@@ -19,6 +22,10 @@ function drawMatrixInput() {
     $("#matrix-data").html(tableMarkup);
 }
 
+/**
+ * Reads the input matrix and returns it as an array.
+ * @return {*} input matrix
+ */
 function readMatrix() {
     var size = parseInt($("#matrix-size").val());
     var M = math.zeros(size, size);
@@ -30,47 +37,43 @@ function readMatrix() {
     return M;
 }
 
+function matrixMarkup(M) {
+    var markup = "";
+    for (var row = 0; row < M.length; row++) {
+        markup += "<tr>";
+        for (var col = 0; col < M.length; col++) {
+            markup += '<td>' + M[row][col] + '</td>';
+        }
+        markup += "</tr>";
+    }
+    return markup;
+}
+
+/**
+ * Presents the decomposition.
+ */
 function presentDecomposition() {
     var shouldLPU = $("#lpu-decomp").prop("checked");
-    var size = parseInt($("#matrix-size").val());
     var result = decompose(readMatrix(), shouldLPU);
     var markup = "";
 
+    // if the checkbox is checked, shows the P matrix.
     if (shouldLPU) {
         $("#P-div").show();
-        for (var row = 0; row < size; row++) {
-            markup += "<tr>";
-            for (var col = 0; col < size; col++) {
-                markup += '<td>' + result[P_CELL][row][col] + '</td>';
-            }
-            markup += "</tr>";
-        }
-        $("#P-matrix").html(markup);
+        $("#P-matrix").html(matrixMarkup(result[P_CELL]));
     }
     else {
         $("#P-div").hide();
     }
 
-    markup = "";
+    // shows each step of the decomposition process
     for (var step = 1; step < result[LOG_L_CELL].length; step++) {
         markup += "<div><h3 class='clear'>Step " + step + "</h3>";
         markup += "<div class='left'><h4>L Matrix</h4><table>";
-        for (var row = 0; row < size; row++) {
-            markup += "<tr>";
-            for (var col = 0; col < size; col++) {
-                markup += '<td>' + result[LOG_L_CELL][step][row][col] + '</td>';
-            }
-            markup += "</tr>";
-        }
+        markup += matrixMarkup(result[LOG_L_CELL][step]);
         markup += "</table></div>";
         markup += "<div class='right'><h4>U Matrix</h4><table>";
-        for (var row = 0; row < size; row++) {
-            markup += "<tr>";
-            for (var col = 0; col < size; col++) {
-                markup += '<td>' + result[LOG_U_CELL][step][row][col] + '</td>';
-            }
-            markup += "</tr>";
-        }
+        markup += matrixMarkup(result[LOG_U_CELL][step]);
         markup += "</table></div></div><br>";
     }
 

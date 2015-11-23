9bf37fbbb7c2cf641f436f2116f475b3ed235cdd
avivyaish <aviv.yaish@mail.huji.ac.il>
2015-11-23 13:26:51 +0200

    Start of tests

---
+++ scripts/tests.js
@@ -0,0 +1,9 @@
+//var testMatrix1 = [[1, 3, -1], [0, 1, 7]];
+//
+//var m = [
+//  [ 1, 2, -1, -4],
+//  [ 2, 3, -1,-11],
+//  [-2, 0, -3, 22]
+//];
+//
+//window.alert(matrixToStr(decomposeRREF(testMatrix1)));
